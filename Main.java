5b9c64412cf8178d1f62c845e2434f2342f7f3b9
Osamaseid <119799518+Osamaseid@users.noreply.github.com>
2023-02-13 21:52:57 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
-# Java-Project About Property Management System
+# Java-Project About shop Property Management System
+ This system solve so many problems that can happen in the shop
+ It can  manage and tell *number of items that shop have
+                         *number of items that are sold
+                         *number of item that are unsold
+                         *about the price of each item
+                         *and it have password to secure the system and this help the owner of this system to manage and know how items are sold. 
+                        
+
 GROUP MEMBERS NAME      ID
 OSAMA SEID             RU2911/13
 ABDULHAFIZ MOHAMMED    RU0206/13
